cdec12acf875af180604194957ca35da00db58e4
Pilar Marinas <pilar.marinas@upcnet.es>
2023-09-12 10:10:48 +0200

    Preparing release 1.0

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ long_description = "\n\n".join(
 
 setup(
     name="collective.easyformplugin.createdx",
-    version="1.0a4.dev0",
+    version="1.0",
     description="Create dexterity objects from easyform submissions",
     long_description=long_description,
     # Get more from https://pypi.org/classifiers/
