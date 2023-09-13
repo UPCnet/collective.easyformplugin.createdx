58b471506c5a49ac0cbb56642ed5af27afbaf738
Ivan Kosovtsev <74710274+r-ivkos@users.noreply.github.com>
2023-09-13 13:36:14 +0200

    Change python requires version to >=2.7

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ setup(
     package_dir={"": "src"},
     include_package_data=True,
     zip_safe=False,
-    python_requires="==2.7, >=3.11.1",
+    python_requires=">=2.7",
     install_requires=[
         "setuptools",
         # -*- Extra requirements: -*-
